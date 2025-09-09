2ae823af79027b13610337e68d72ec5e608734ac
ArneC <christiansarne04@gmail.com>
2025-09-09 16:28:32 +0200

    Implement repeatString function and enable tests
    Added implementation for repeatString to repeat a string n times or return 'ERROR' for negative numbers. Enabled previously skipped tests in repeatString.spec.js to verify correct behavior.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringNtimes = "";
+    if(num >= 0) {
+        for(let i = 0; num > i; i++) {
+            stringNtimes += string;
+        }
+    } else {
+        return "ERROR";
+    }
 
+    return stringNtimes;
 };
 
 // Do not edit below this line
